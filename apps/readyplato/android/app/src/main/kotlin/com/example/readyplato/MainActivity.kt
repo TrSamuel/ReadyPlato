fe5d174ca0f78b8c077dbaa1dfd4415e48e7b9cd
Samuel Raju <trsamual@gmail.com>
2026-04-28 19:28:30 +0530

    project created

---
+++ apps/readyplato/android/app/src/main/kotlin/com/example/readyplato/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.readyplato
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ apps/readyplato_business/android/app/src/main/kotlin/com/example/readyplato_business/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.readyplato_business
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
